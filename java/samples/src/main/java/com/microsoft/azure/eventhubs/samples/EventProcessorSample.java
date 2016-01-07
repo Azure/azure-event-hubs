29a629366a4abe6ef469f1642c71a52278808f2f
JamesBirdsall <jbird@microsoft.com>
2016-01-06 17:59:45 -0800

    A couple more lines of sample

--- java/samples/src/main/java/com/microsoft/azure/eventhubs/samples/EventProcessorSample.java
+++ java/samples/src/main/java/com/microsoft/azure/eventhubs/samples/EventProcessorSample.java
@@ -8,5 +8,8 @@ public class EventProcessorSample {
     public static void main(String args[])
     {
         ConnectionStringBuilder connStr = new ConnectionStringBuilder("jbirdtest", "learnhub", "allpolicy", "SvgdDB9qala2yLA4YGc/cjxTr52W5zBPpAGYahGEIto=");
+        AzureStorageCheckpointLeaseManager combinedManager = new AzureStorageCheckpointLeaseManager("TODO");
+        //combinedManager.InitializeCheckpointManager("jbirdTest", "$Default",);
+        EventProcessorHost host = new EventProcessorHost(connStr.toString(), "$Default", combinedManager, combinedManager);
     }
 }
