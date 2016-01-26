f99a2fe94ad350537d96b46467d99e2f2e99db9f
JamesBirdsall <jbird@microsoft.com>
2016-01-26 15:04:29 -0800

    Pump/partition fixes and tuning

--- java/src/main/java/com/microsoft/azure/eventprocessorhost/PartitionManager.java
+++ java/src/main/java/com/microsoft/azure/eventprocessorhost/PartitionManager.java
@@ -13,7 +13,6 @@ public class PartitionManager
 
     // DUMMY STARTS
     public static int dummyPartitionCount = 4;
-    public static int dummyHostCount = 1;
     // DUMMY ENDS
 
     public PartitionManager(EventProcessorHost host)
@@ -36,7 +35,7 @@ public class PartitionManager
 
         return this.partitionIds;
     }
-
+    
     public HashMap<String, Lease> getSomeLeases() throws Exception
     {
         ILeaseManager leaseManager = this.host.getLeaseManager();
@@ -69,16 +68,14 @@ public class PartitionManager
             // Inspect all leases.
             // Take any leases that currently belong to us.
             // If any are expired, take those.
-            // TODO Grab more if needed for load balancing
+            // Grab more if needed for load balancing
 
             Iterable<Future<Lease>> allLeases = leaseManager.getAllLeases();
-            int allLeasesCount = 0;
             int ourLeasesCount = 0;
             ArrayList<Lease> somebodyElsesLeases = new ArrayList<Lease>();
             for (Future<Lease> future : allLeases)
             {
                 Lease possibleLease = future.get();
-                allLeasesCount++;
                 if ((possibleLease.getOwner().compareTo(this.host.getHostName()) == 0) || possibleLease.isExpired())
                 {
                     Lease gotLease = leaseManager.acquireLease(possibleLease.getPartitionId()).get();
@@ -93,23 +90,30 @@ public class PartitionManager
                 	somebodyElsesLeases.add(possibleLease);
                 }
             }
-            // DUMMY STARTS
-            while ((ourLeasesCount < (allLeasesCount / PartitionManager.dummyHostCount)) && (somebodyElsesLeases.size() > 0))
+
+            if (somebodyElsesLeases.size() > 0)
             {
-            	Lease tryTake = somebodyElsesLeases.remove(0);
-            	Lease takenLease = leaseManager.acquireLease(tryTake.getPartitionId()).get();
-            	if (takenLease != null)
-            	{
-            		this.host.logWithHostAndPartition(takenLease.getPartitionId(), "Stole lease");
-            		ourLeases.put(takenLease.getPartitionId(), takenLease);
-            		ourLeasesCount++;
-            	}
-            	else
-            	{
-            		this.host.logWithHost("Failed to steal lease for partition " + tryTake.getPartitionId());
-            	}
+	            Iterable<Lease> stealTheseLeases = whichLeasesToSteal(somebodyElsesLeases, ourLeasesCount);
+	            if (stealTheseLeases != null)
+	            {
+	            	for (Lease stealee : stealTheseLeases)
+	            	{
+	            		Lease takenLease = leaseManager.acquireLease(stealee.getPartitionId()).get();
+	                	if (takenLease != null)
+	                	{
+	                		this.host.logWithHostAndPartition(takenLease.getPartitionId(), "Stole lease");
+	                		ourLeases.put(takenLease.getPartitionId(), takenLease);
+	                		ourLeasesCount++;
+	                	}
+	                	else
+	                	{
+	                		this.host.logWithHost("Failed to steal lease for partition " + stealee.getPartitionId());
+	                	}
+	            	}
+	            }
             }
-            
+
+            // DUMMY BEGINS
             allLeases = leaseManager.getAllLeases();
             for (Future<Lease> future : allLeases)
             {
@@ -120,5 +124,78 @@ public class PartitionManager
         }
 
         return ourLeases;
+    }
+    
+    private Iterable<Lease> whichLeasesToSteal(ArrayList<Lease> stealableLeases, int haveLeaseCount)
+    {
+    	HashMap<String, Integer> countsByOwner = countLeasesByOwner(stealableLeases);
+    	int totalLeases = stealableLeases.size() + haveLeaseCount;
+    	int hostCount = countsByOwner.size() + 1;
+    	int desiredToHave = (totalLeases + hostCount - 1) / hostCount; // round up
+    	ArrayList<Lease> stealTheseLeases = null;
+    	if ((desiredToHave - haveLeaseCount) == 1)
+    	{
+    		this.host.logWithHost("Have only one less than desired, skipping lease stealing");
+    	}
+    	else if (haveLeaseCount < desiredToHave)
+    	{
+    		this.host.logWithHost("Has " + haveLeaseCount + " leases, wants " + desiredToHave);
+    		stealTheseLeases = new ArrayList<Lease>();
+    		String stealFrom = findBiggestOwner(countsByOwner);
+    		this.host.logWithHost("Proposed to steal leases from " + stealFrom);
+    		for (Lease l : stealableLeases)
+    		{
+    			if (l.getOwner().compareTo(stealFrom) == 0)
+    			{
+    				stealTheseLeases.add(l);
+    				this.host.logWithHost("Proposed to steal lease for partition " + l.getPartitionId());
+    				haveLeaseCount++;
+    				if (haveLeaseCount >= desiredToHave)
+    				{
+    					break;
+    				}
+    			}
+    		}
+    	}
+    	return stealTheseLeases;
+    }
+    
+    private String findBiggestOwner(HashMap<String, Integer> countsByOwner)
+    {
+    	int biggestCount = 0;
+    	String biggestOwner = null;
+    	for (String owner : countsByOwner.keySet())
+    	{
+    		if (countsByOwner.get(owner) > biggestCount)
+    		{
+    			biggestCount = countsByOwner.get(owner);
+    			biggestOwner = owner;
+    		}
+    	}
+    	return biggestOwner;
+    }
+    
+    private HashMap<String, Integer> countLeasesByOwner(Iterable<Lease> leases)
+    {
+    	HashMap<String, Integer> counts = new HashMap<String, Integer>();
+    	for (Lease l : leases)
+    	{
+    		if (counts.containsKey(l.getOwner()))
+    		{
+    			Integer oldCount = counts.get(l.getOwner());
+    			counts.put(l.getOwner(), oldCount + 1);
+    		}
+    		else
+    		{
+    			counts.put(l.getOwner(), 1);
+    		}
+    	}
+    	for (String owner : counts.keySet())
+    	{
+    		this.host.log("host " + owner + " owns " + counts.get(owner) + " leases");
+    	}
+    	this.host.log("total hosts in sorted list: " + counts.size());
+    	
+    	return counts;
     }
 }
