f4014e25001ed784f0125f89599e861eca7acfaa
JamesBirdsall <jbird@microsoft.com>
2016-03-14 19:39:36 -0700

    Debug and test of AzureStorageLeaseCheckpointManager

--- java/azure-eventhubs-samples/src/main/java/com/microsoft/azure/eventhubs/samples/EventProcessorSample.java
+++ java/azure-eventhubs-samples/src/main/java/com/microsoft/azure/eventhubs/samples/EventProcessorSample.java
@@ -3,20 +3,58 @@ package com.microsoft.azure.eventhubs.samples;
 
 import com.microsoft.azure.eventhubs.EventData;
 import com.microsoft.azure.eventprocessorhost.*;
+import com.microsoft.azure.storage.StorageException;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class EventProcessorSample {
     public static void main(String args[])
     {
-    	int hostCount = 2;
-    	EventProcessorHost.setDummyPartitionCount(4);
+    	final int hostCount = 1;
+    	final int partitionCount = 8;
+    	EventProcessorHost.setDummyPartitionCount(partitionCount);
     	
-    	EventProcessorHost[] hosts = new EventProcessorHost[hostCount];
+    	if (true)
+    	{
+	    	//InMemoryCheckpointLeaseManager mgr = new InMemoryCheckpointLeaseManager();
+    		AzureStorageCheckpointLeaseManager mgr = new AzureStorageCheckpointLeaseManager("storage connection string");
+	    	EventProcessorHost blah = new EventProcessorHost("namespace", "eventhub", "keyname", "key", "$Default", mgr, mgr);
+	    	try
+	    	{
+				mgr.initialize(blah);
+			}
+	    	catch (Exception e)
+	    	{
+	    		System.out.println("Initialize failed " + e.toString());
+	    		e.printStackTrace();
+			}
+	    	
+	    	basicLeaseManagerTest(mgr, partitionCount);
+    	}
+    	else if (false)
+    	{
+    		EventProcessorHost[] hosts = new EventProcessorHost[hostCount];
+    		for (int i = 0; i < hostCount; i++)
+    		{
+    			hosts[i] = new EventProcessorHost("namespace", "eventhub", "keyname", "key", "$Default", "storage connection string");
+    		}
+    		processMessages(hosts);
+    	}
+    	
+        System.out.println("Exiting");
+    }
+    
+    private static void processMessages(EventProcessorHost[] hosts)
+    {
+    	int hostCount = hosts.length;
     	
     	for (int i = 0; i < hostCount; i++)
     	{
-    		hosts[i] = new EventProcessorHost("namespace", "eventhub", "keyname", "key", "$Default", "storage connection string");
     		System.out.println("Registering host " + i + " named " + hosts[i].getHostName());
     		hosts[i].registerEventProcessor(EventProcessor.class);
     		try
@@ -25,8 +63,7 @@ public class EventProcessorSample {
     		}
     		catch (InterruptedException e1)
     		{
-    			// TODO Auto-generated catch block
-    			e1.printStackTrace();
+    			// Watch me not care
     		}
     	}
 
@@ -48,10 +85,156 @@ public class EventProcessorSample {
             System.out.println(e.toString());
             e.printStackTrace();
         }
-
-        System.out.println("Exiting");
     }
+    
+    private static void basicLeaseManagerTest(ILeaseManager mgr, int partitionCount)
+    {
+    	try
+    	{
+        	System.out.println("Store should not exist");
+			Boolean boolret = mgr.leaseStoreExists().get();
+			System.out.println("getStoreExists() returned " + boolret);
+			
+			System.out.println("Creating store");
+			boolret = mgr.createLeaseStoreIfNotExists().get();
+			System.out.println("createStoreIfNotExists() returned " + boolret);
 
+        	System.out.println("Store should exist now");
+			boolret = mgr.leaseStoreExists().get();
+			System.out.println("getStoreExists() returned " + boolret);
+			
+			Lease[] leases = new Lease[partitionCount];
+			for (Integer i = 0; i < partitionCount; i++)
+			{
+				System.out.print("Creating lease for partition " + i + "... ");
+				Lease createdLease = mgr.createLeaseIfNotExists(i.toString()).get();
+				leases[i] = createdLease;
+				System.out.println("OK");
+			}
+			
+			for (int i = 0; i < partitionCount; i++)
+			{
+				System.out.println("Partition " + i + " state before: " + ((AzureBlobLease)leases[i]).getState());
+				System.out.print("Acquiring lease for partition " + i + "... ");
+				boolret = mgr.acquireLease(leases[i]).get();
+				System.out.println(boolret.toString());
+				System.out.println("Partition " + i + " state after: " + ((AzureBlobLease)leases[i]).getState());
+			}
+			
+			System.out.print("Sleeping... ");
+			Thread.sleep(5000);
+			System.out.println("done");
+			
+			for (int i = 0; i < partitionCount; i++)
+			{
+				System.out.println("Partition " + i + " state before: " + ((AzureBlobLease)leases[i]).getState());
+				System.out.print("Renewing lease for partition " + i + "... ");
+				boolret = mgr.renewLease(leases[i]).get();
+				System.out.println(boolret.toString());
+				System.out.println("Partition " + i + " state after: " + ((AzureBlobLease)leases[i]).getState());
+			}
+			
+			System.out.println("Waiting for lease on 0 to expire.");
+			int x = 1;
+			while (!leases[0].isExpired())
+			{
+				Thread.sleep(5000);
+				System.out.println("Still waiting for lease on 0 to expire: " + (5 * x++));
+				for (int i = 1; i < partitionCount; i++)
+				{
+					System.out.print("   Renewing lease for partition " + i + "... ");
+					boolret = mgr.renewLease(leases[i]).get();
+					System.out.println(boolret.toString());
+				}
+			}
+			System.out.println("Expired!");
+			
+			for (int i = 0; i < partitionCount; i++)
+			{
+				System.out.println("Partition " + i + " state before: " + ((AzureBlobLease)leases[i]).getState());
+				System.out.print("Releasing lease for partition " + i + "... ");
+				try
+				{
+					boolret = mgr.releaseLease(leases[i]).get();
+					System.out.println(boolret.toString());
+				}
+				catch (ExecutionException lle)
+				{
+					if (lle.getCause() instanceof LeaseLostException)
+					{
+						System.out.println("caught LeaseLostException");
+					}
+					else
+					{
+						throw lle;
+					}
+				}
+				System.out.println("Partition " + i + " state after: " + ((AzureBlobLease)leases[i]).getState());
+			}
+    	}
+    	catch (Exception e)
+    	{
+        	System.out.println("Caught " + e.toString());
+        	StackTraceElement[] stack = e.getStackTrace();
+        	for (int i = 0; i < stack.length; i++)
+        	{
+        		System.out.println(stack[i].toString());
+        	}
+		}
+    }
+    
+    public class SyntheticPump extends PartitionPump
+    {
+    	Future<Void> producer = null;
+    	boolean keepGoing = true;
+    	
+		@Override
+		public void specializedStartPump()
+		{
+			this.producer = EventProcessorHost.getExecutorService().submit(() -> produceMessages());
+		}
+
+		@Override
+		public void specializedShutdown(CloseReason reason)
+		{
+			this.keepGoing = false;
+			try
+			{
+				this.producer.get();
+			}
+			catch (InterruptedException | ExecutionException e)
+			{
+				System.out.println("SyntheticPump shutdown failure" + e.toString());
+				e.printStackTrace();
+			}
+		}
+		
+		private Void produceMessages()
+		{
+			ArrayList<EventData> events = new ArrayList<EventData>();
+			int eventNumber = 0;
+			
+			while (this.keepGoing)
+			{
+				events.clear();
+				String eventBody = "Event " + eventNumber + " on partition " + this.lease.getPartitionId();
+				EventData event = new EventData(eventBody.getBytes());
+				events.add(event);
+				onEvents(events);
+				
+				try
+				{
+					Thread.sleep(3000);
+				}
+				catch (InterruptedException e)
+				{
+					// Watch me not care
+				}
+			}
+			
+			return null;
+		}
+    }
 
     public static class EventProcessor implements IEventProcessor
     {
