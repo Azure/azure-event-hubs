9f7ead654ab26d464919f174e2d2e52afd32f047
JamesBirdsall <jbird@microsoft.com>
2016-03-24 18:43:39 -0700

    Dummy change for code review

--- java/event-processor-host/src/main/java/com/microsoft/azure/eventprocessorhost/DefaultEventProcessorFactory.java
+++ java/event-processor-host/src/main/java/com/microsoft/azure/eventprocessorhost/DefaultEventProcessorFactory.java
@@ -3,6 +3,8 @@
  * Licensed under the MIT license. See LICENSE file in the project root for full license information.
  */
 
+// BLAH
+
 package com.microsoft.azure.eventprocessorhost;
 
 
--- java/event-processor-host/src/main/java/com/microsoft/azure/eventprocessorhost/TemporaryTest.java
+++ java/event-processor-host/src/main/java/com/microsoft/azure/eventprocessorhost/TemporaryTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+// BLAH
+
 package com.microsoft.azure.eventprocessorhost;
 
 import java.util.ArrayList;
