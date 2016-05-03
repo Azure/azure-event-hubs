ef65babbc8bb5e2e2ca6b870c1b8ff6f47f72405
David Stucki <david.stucki@microsoft.com>
2016-05-02 19:30:22 -0700

    Adding a unit test console application for testing purposes.

---
+++ csharp/test/Microsoft.Azure.EventHubs.UnitTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.EventHubs.UnitTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString;
+            if (args.Length == 0 || string.IsNullOrEmpty((connectionString = args[0])))
+            {
+                Console.Write("Please enter a connection string which includes an event hub name:");
+                connectionString = Console.ReadLine().Trim();
+            }
+
+            TestRunner.RunAsync(() => EventHubClientTests.RunAsync(connectionString)).Wait();
+
+            TestRunner.Done();
+            Console.ReadLine();
+        }
+    }
+}
