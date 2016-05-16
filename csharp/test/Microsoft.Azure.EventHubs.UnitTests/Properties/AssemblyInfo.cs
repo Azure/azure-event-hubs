e58c2b79546f7892b08a45b71776e0293fb5f4ce
David Stucki <david.stucki@microsoft.com>
2016-05-16 15:50:28 -0700

    Move from DNX RC1 to .NET Core RC2.

--- csharp/test/Microsoft.Azure.EventHubs.UnitTests/Properties/AssemblyInfo.cs
+++ csharp/test/Microsoft.Azure.EventHubs.UnitTests/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.EventHubs.UnitTests")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.Azure.EventHubs.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5dfe7651-3a46-44a5-a49d-0ee11942c492")]
+[assembly: Guid("154f7b4c-b998-4fa0-933f-f34db0ca9b88")]
