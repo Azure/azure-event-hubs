433d6f666118c4a9be87149caad2ae53b23b6b3b
Sreeram Kumar Garlapati <sreeramg@microsoft.com>
2016-08-26 15:14:24 -0700

    resending EventData with special properties shoudln't lose any of them

--- java/azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/eventdata/InteropTest.java
+++ java/azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/eventdata/InteropTest.java
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 import org.apache.qpid.proton.Proton;
 import org.apache.qpid.proton.amqp.Binary;
@@ -21,6 +22,7 @@ import org.junit.Test;
 import com.microsoft.azure.eventhubs.EventData;
 import com.microsoft.azure.eventhubs.EventHubClient;
 import com.microsoft.azure.eventhubs.PartitionReceiver;
+import com.microsoft.azure.eventhubs.PartitionSender;
 import com.microsoft.azure.eventhubs.lib.ApiTestBase;
 import com.microsoft.azure.eventhubs.lib.TestContext;
 import com.microsoft.azure.servicebus.ConnectionStringBuilder;
@@ -34,9 +36,10 @@ public class InteropTest extends ApiTestBase
 	static EventHubClient ehClient;
 	static MessagingFactory msgFactory;
 	static PartitionReceiver receiver;
-	static MessageSender partitionSender;
+	static MessageSender partitionMsgSender;
+	static PartitionSender partitionEventSender;
 
-	static String partitionId = "0";
+	static final String partitionId = "0";
 
 	@BeforeClass
 	public static void initialize() throws ServiceBusException, IOException, InterruptedException, ExecutionException
@@ -47,7 +50,8 @@ public class InteropTest extends ApiTestBase
 		ehClient = EventHubClient.createFromConnectionStringSync(connectionString);
 		msgFactory = MessagingFactory.createFromConnectionString(connectionString).get();
 		receiver = ehClient.createReceiverSync(TestContext.getConsumerGroupName(), partitionId, Instant.now());
-		partitionSender = MessageSender.create(msgFactory, "link1", connStrBuilder.getEntityPath() + "/partitions/" + partitionId).get();
+		partitionMsgSender = MessageSender.create(msgFactory, "link1", connStrBuilder.getEntityPath() + "/partitions/" + partitionId).get();
+		partitionEventSender = ehClient.createPartitionSenderSync(partitionId);
 	}
 
 	@Test
@@ -82,50 +86,62 @@ public class InteropTest extends ApiTestBase
 		final String payload = "testmsg";
 		protonMessage.setBody(new Data(Binary.create(ByteBuffer.wrap(payload.getBytes()))));
 		
-		partitionSender.send(protonMessage).get();
+		partitionMsgSender.send(protonMessage).get();
+		final EventData receivedEvent = receiver.receiveSync(10).iterator().next();
 		
-		EventData receivedEvent = receiver.receiveSync(10).iterator().next();
-		
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID).equals(protonMessage.getMessageId()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_USER_ID)
-				&& new String((byte[]) receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_USER_ID)).equals(new String(protonMessage.getUserId())));		
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_TO)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_TO).equals(protonMessage.getAddress()));		
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE).equals(protonMessage.getContentType()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING).equals(protonMessage.getContentEncoding()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID).equals(protonMessage.getCorrelationId()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CREATION_TIME)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CREATION_TIME).equals(protonMessage.getCreationTime()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_SUBJECT)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_SUBJECT).equals(protonMessage.getSubject()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_GROUP_ID)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_GROUP_ID).equals(protonMessage.getGroupId()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID).equals(protonMessage.getReplyToGroupId()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO).equals(protonMessage.getReplyTo()));
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time)
-				&& receivedEvent.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time).equals(protonMessage.getExpiryTime()));
-		
-		
-		Assert.assertTrue(receivedEvent.getSystemProperties().containsKey(msgAnnotation)
-				&& receivedEvent.getSystemProperties().get(msgAnnotation).equals(protonMessage.getMessageAnnotations().getValue().get(Symbol.getSymbol(msgAnnotation))));
-		
-		Assert.assertTrue(receivedEvent.getProperties().containsKey(applicationProperty)
-				&& receivedEvent.getProperties().get(applicationProperty).equals(protonMessage.getApplicationProperties().getValue().get(applicationProperty)));
-		
-		Assert.assertTrue(new String(receivedEvent.getBody()).equals(payload));
+		final Consumer<EventData> eventValidator = new Consumer<EventData>()
+		{
+			@Override
+			public void accept(EventData eData)
+			{
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID).equals(protonMessage.getMessageId()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_USER_ID)
+						&& new String((byte[]) eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_USER_ID)).equals(new String(protonMessage.getUserId())));		
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_TO)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_TO).equals(protonMessage.getAddress()));		
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE).equals(protonMessage.getContentType()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING).equals(protonMessage.getContentEncoding()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID).equals(protonMessage.getCorrelationId()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CREATION_TIME)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CREATION_TIME).equals(protonMessage.getCreationTime()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_SUBJECT)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_SUBJECT).equals(protonMessage.getSubject()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_GROUP_ID)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_GROUP_ID).equals(protonMessage.getGroupId()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID).equals(protonMessage.getReplyToGroupId()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO).equals(protonMessage.getReplyTo()));
+				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time)
+						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time).equals(protonMessage.getExpiryTime()));
+				
+				
+				Assert.assertTrue(eData.getSystemProperties().containsKey(msgAnnotation)
+						&& eData.getSystemProperties().get(msgAnnotation).equals(protonMessage.getMessageAnnotations().getValue().get(Symbol.getSymbol(msgAnnotation))));
+				
+				Assert.assertTrue(eData.getProperties().containsKey(applicationProperty)
+						&& eData.getProperties().get(applicationProperty).equals(protonMessage.getApplicationProperties().getValue().get(applicationProperty)));
+				
+				Assert.assertTrue(new String(eData.getBody()).equals(payload));	
+			}};
+	
+			eventValidator.accept(receivedEvent);
+			
+			partitionEventSender.sendSync(receivedEvent);
+			EventData reSentAndReceivedEvent = receiver.receiveSync(10).iterator().next();
+			
+			eventValidator.accept(reSentAndReceivedEvent);
 	}
 	
 	@AfterClass
 	public static void cleanup() throws ServiceBusException
 	{
-		if (partitionSender != null)
-			partitionSender.closeSync();
+		if (partitionMsgSender != null)
+			partitionMsgSender.closeSync();
 		
 		if (receiver != null)
 			receiver.closeSync();
