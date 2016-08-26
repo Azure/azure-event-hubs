1a95e071b56f60590ff545bb068a2a4c70d1fa85
Sreeram Kumar Garlapati <sreeramg@microsoft.com>
2016-08-26 16:26:39 -0700

    send a protonMsg to EventHub & receive as EventData & resend to EventHub and then receive it as AmqpMessage. This final msg shouldb be same as the original one.

--- java/azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/eventdata/InteropTest.java
+++ java/azure-eventhubs/src/test/java/com/microsoft/azure/eventhubs/eventdata/InteropTest.java
@@ -26,6 +26,7 @@ import com.microsoft.azure.eventhubs.PartitionSender;
 import com.microsoft.azure.eventhubs.lib.ApiTestBase;
 import com.microsoft.azure.eventhubs.lib.TestContext;
 import com.microsoft.azure.servicebus.ConnectionStringBuilder;
+import com.microsoft.azure.servicebus.MessageReceiver;
 import com.microsoft.azure.servicebus.MessageSender;
 import com.microsoft.azure.servicebus.MessagingFactory;
 import com.microsoft.azure.servicebus.ServiceBusException;
@@ -36,11 +37,62 @@ public class InteropTest extends ApiTestBase
 	static EventHubClient ehClient;
 	static MessagingFactory msgFactory;
 	static PartitionReceiver receiver;
+	static MessageReceiver msgReceiver;
 	static MessageSender partitionMsgSender;
 	static PartitionSender partitionEventSender;
 
 	static final String partitionId = "0";
-
+	
+	static final Message originalMessage = Proton.message();
+	static final String applicationProperty = "firstProp";
+	static final String msgAnnotation = "message-annotation-1";
+	static final String payload = "testmsg";
+	
+	static EventData receivedEvent;
+	static EventData reSentAndReceivedEvent;
+	static Message reSendAndReceivedMessage;
+	
+	final Consumer<EventData> validateAmqpPropertiesInEventData = new Consumer<EventData>()
+	{
+		@Override
+		public void accept(EventData eData)
+		{
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID).equals(originalMessage.getMessageId()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_USER_ID)
+					&& new String((byte[]) eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_USER_ID)).equals(new String(originalMessage.getUserId())));		
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_TO)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_TO).equals(originalMessage.getAddress()));		
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE).equals(originalMessage.getContentType()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING).equals(originalMessage.getContentEncoding()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID).equals(originalMessage.getCorrelationId()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CREATION_TIME)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CREATION_TIME).equals(originalMessage.getCreationTime()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_SUBJECT)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_SUBJECT).equals(originalMessage.getSubject()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_GROUP_ID)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_GROUP_ID).equals(originalMessage.getGroupId()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID).equals(originalMessage.getReplyToGroupId()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO).equals(originalMessage.getReplyTo()));
+			Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time)
+					&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time).equals(originalMessage.getExpiryTime()));
+			
+			Assert.assertTrue(eData.getSystemProperties().containsKey(msgAnnotation)
+					&& eData.getSystemProperties().get(msgAnnotation).equals(originalMessage.getMessageAnnotations().getValue().get(Symbol.getSymbol(msgAnnotation))));
+			
+			Assert.assertTrue(eData.getProperties().containsKey(applicationProperty)
+					&& eData.getProperties().get(applicationProperty).equals(originalMessage.getApplicationProperties().getValue().get(applicationProperty)));
+			
+			Assert.assertTrue(eData.getProperties().size() == 1);
+			
+			Assert.assertTrue(new String(eData.getBody(), eData.getBodyOffset(), eData.getBodyLength()).equals(payload));	
+		}};
+		
 	@BeforeClass
 	public static void initialize() throws ServiceBusException, IOException, InterruptedException, ExecutionException
 	{
@@ -52,94 +104,95 @@ public class InteropTest extends ApiTestBase
 		receiver = ehClient.createReceiverSync(TestContext.getConsumerGroupName(), partitionId, Instant.now());
 		partitionMsgSender = MessageSender.create(msgFactory, "link1", connStrBuilder.getEntityPath() + "/partitions/" + partitionId).get();
 		partitionEventSender = ehClient.createPartitionSenderSync(partitionId);
-	}
-
-	@Test
-	public void interopWithDirectProtonAmqpMessage() throws ServiceBusException, InterruptedException, ExecutionException
-	{
-		final Message protonMessage = Proton.message();
 		
-		final String applicationProperty = "firstProp";
 		final HashMap<String, String> appProperties = new HashMap<String, String>();
 		appProperties.put(applicationProperty, "value1");
 		final ApplicationProperties applicationProperties = new ApplicationProperties(appProperties);
-		protonMessage.setApplicationProperties(applicationProperties);
+		originalMessage.setApplicationProperties(applicationProperties);
 		
-		protonMessage.setMessageId("id1");
-		protonMessage.setUserId("user1".getBytes());
-		protonMessage.setAddress("eventhub1");
-		protonMessage.setSubject("sub");
-		protonMessage.setReplyTo("replyingTo");
-		protonMessage.setExpiryTime(456L);
-		protonMessage.setGroupSequence(5555L);
-		protonMessage.setContentType("events");
-		protonMessage.setContentEncoding("UTF-8");
-		protonMessage.setCorrelationId("corid1");
-		protonMessage.setCreationTime(345L);
-		protonMessage.setGroupId("gid");
-		protonMessage.setReplyToGroupId("replyToGroupId");
+		originalMessage.setMessageId("id1");
+		originalMessage.setUserId("user1".getBytes());
+		originalMessage.setAddress("eventhub1");
+		originalMessage.setSubject("sub");
+		originalMessage.setReplyTo("replyingTo");
+		originalMessage.setExpiryTime(456L);
+		originalMessage.setGroupSequence(5555L);
+		originalMessage.setContentType("events");
+		originalMessage.setContentEncoding("UTF-8");
+		originalMessage.setCorrelationId("corid1");
+		originalMessage.setCreationTime(345L);
+		originalMessage.setGroupId("gid");
+		originalMessage.setReplyToGroupId("replyToGroupId");
 		
-		final String msgAnnotation = "message-annotation-1";
-		protonMessage.setMessageAnnotations(new MessageAnnotations(new HashMap<Symbol, Object>()));
-		protonMessage.getMessageAnnotations().getValue().put(Symbol.getSymbol(msgAnnotation), "messageAnnotationValue");
+		originalMessage.setMessageAnnotations(new MessageAnnotations(new HashMap<Symbol, Object>()));
+		originalMessage.getMessageAnnotations().getValue().put(Symbol.getSymbol(msgAnnotation), "messageAnnotationValue");
 		
-		final String payload = "testmsg";
-		protonMessage.setBody(new Data(Binary.create(ByteBuffer.wrap(payload.getBytes()))));
+		originalMessage.setBody(new Data(Binary.create(ByteBuffer.wrap(payload.getBytes()))));
 		
-		partitionMsgSender.send(protonMessage).get();
-		final EventData receivedEvent = receiver.receiveSync(10).iterator().next();
+		partitionMsgSender.send(originalMessage).get();
+		receivedEvent = receiver.receiveSync(10).iterator().next();
 		
-		final Consumer<EventData> eventValidator = new Consumer<EventData>()
-		{
-			@Override
-			public void accept(EventData eData)
-			{
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_MESSAGE_ID).equals(protonMessage.getMessageId()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_USER_ID)
-						&& new String((byte[]) eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_USER_ID)).equals(new String(protonMessage.getUserId())));		
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_TO)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_TO).equals(protonMessage.getAddress()));		
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_TYPE).equals(protonMessage.getContentType()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CONTENT_ENCODING).equals(protonMessage.getContentEncoding()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CORRELATION_ID).equals(protonMessage.getCorrelationId()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_CREATION_TIME)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_CREATION_TIME).equals(protonMessage.getCreationTime()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_SUBJECT)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_SUBJECT).equals(protonMessage.getSubject()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_GROUP_ID)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_GROUP_ID).equals(protonMessage.getGroupId()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO_GROUP_ID).equals(protonMessage.getReplyToGroupId()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_REPLY_TO)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_REPLY_TO).equals(protonMessage.getReplyTo()));
-				Assert.assertTrue(eData.getSystemProperties().containsKey(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time)
-						&& eData.getSystemProperties().get(AmqpConstants.AMQP_PROPERTY_ABSOLUTE_EXPRITY_time).equals(protonMessage.getExpiryTime()));
-				
-				
-				Assert.assertTrue(eData.getSystemProperties().containsKey(msgAnnotation)
-						&& eData.getSystemProperties().get(msgAnnotation).equals(protonMessage.getMessageAnnotations().getValue().get(Symbol.getSymbol(msgAnnotation))));
-				
-				Assert.assertTrue(eData.getProperties().containsKey(applicationProperty)
-						&& eData.getProperties().get(applicationProperty).equals(protonMessage.getApplicationProperties().getValue().get(applicationProperty)));
-				
-				Assert.assertTrue(new String(eData.getBody()).equals(payload));	
-			}};
+		partitionEventSender.sendSync(receivedEvent);
+		reSentAndReceivedEvent = receiver.receiveSync(10).iterator().next();
+		
+		partitionEventSender.sendSync(reSentAndReceivedEvent);
+		msgReceiver = MessageReceiver.create(
+				msgFactory, 
+				"receiver1", 
+				connStrBuilder.getEntityPath() + "/ConsumerGroups/" + TestContext.getConsumerGroupName() + "/Partitions/" + partitionId,
+				reSentAndReceivedEvent.getSystemProperties().getOffset(), false, null, 100, 0L, false).get();
+		reSendAndReceivedMessage = msgReceiver.receive(10).get().iterator().next();
+	}
+
+	@Test
+	public void interopWithDirectProtonAmqpMessage()
+	{
+		validateAmqpPropertiesInEventData.accept(receivedEvent);
+	}
 	
-			eventValidator.accept(receivedEvent);
-			
-			partitionEventSender.sendSync(receivedEvent);
-			EventData reSentAndReceivedEvent = receiver.receiveSync(10).iterator().next();
-			
-			eventValidator.accept(reSentAndReceivedEvent);
+	@Test
+	public void interopWithDirectProtonEventDataReSend()
+	{		
+		validateAmqpPropertiesInEventData.accept(reSentAndReceivedEvent);
+	}
+	
+	@Test
+	public void resentAmqpMessageShouldRetainAllOriginalProps()
+	{
+		Assert.assertTrue(reSendAndReceivedMessage.getMessageId().equals(originalMessage.getMessageId()));
+		Assert.assertTrue(reSendAndReceivedMessage.getAddress().equals(originalMessage.getAddress()));
+		Assert.assertTrue(reSendAndReceivedMessage.getContentEncoding().equals(originalMessage.getContentEncoding()));
+		Assert.assertTrue(reSendAndReceivedMessage.getContentType().equals(originalMessage.getContentType()));
+		Assert.assertTrue(new String(reSendAndReceivedMessage.getUserId()).equals(new String(originalMessage.getUserId())));
+		Assert.assertTrue(reSendAndReceivedMessage.getCorrelationId().equals(originalMessage.getCorrelationId()));
+		Assert.assertTrue(reSendAndReceivedMessage.getGroupId().equals(originalMessage.getGroupId()));
+		Assert.assertTrue(reSendAndReceivedMessage.getReplyTo().equals(originalMessage.getReplyTo()));
+		Assert.assertTrue(reSendAndReceivedMessage.getReplyToGroupId().equals(originalMessage.getReplyToGroupId()));
+		Assert.assertTrue(reSendAndReceivedMessage.getSubject().equals(originalMessage.getSubject()));
+		Assert.assertTrue(reSendAndReceivedMessage.getExpiryTime() == originalMessage.getExpiryTime());
+		Assert.assertTrue(reSendAndReceivedMessage.getCreationTime() == originalMessage.getCreationTime());
+		Assert.assertTrue(reSendAndReceivedMessage.getExpiryTime() == originalMessage.getExpiryTime());
+		Assert.assertTrue(reSendAndReceivedMessage.getGroupSequence() == originalMessage.getGroupSequence());
+		
+		Assert.assertTrue(reSendAndReceivedMessage.getApplicationProperties().getValue().get(applicationProperty)
+				.equals(originalMessage.getApplicationProperties().getValue().get(applicationProperty)));
+		
+		Assert.assertTrue(reSendAndReceivedMessage.getMessageAnnotations().getValue().get(Symbol.getSymbol(msgAnnotation))
+				.equals(originalMessage.getMessageAnnotations().getValue().get(Symbol.getSymbol(msgAnnotation))));
+		
+		Binary payloadBytes = ((Data) reSendAndReceivedMessage.getBody()).getValue();
+		Assert.assertTrue(new String(payloadBytes.getArray(), payloadBytes.getArrayOffset(), payloadBytes.getLength()).equals(payload));
 	}
 	
 	@AfterClass
 	public static void cleanup() throws ServiceBusException
 	{
+		if (msgReceiver != null)
+			msgReceiver.closeSync();
+
+		if (partitionEventSender != null)
+			partitionEventSender.closeSync();
+
 		if (partitionMsgSender != null)
 			partitionMsgSender.closeSync();
 		
