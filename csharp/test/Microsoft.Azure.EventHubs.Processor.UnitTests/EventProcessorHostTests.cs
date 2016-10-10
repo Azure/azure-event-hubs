8442ed3346406ab128b7d6ed22d737c5ba2d59e0
Serkant Karaca <serkar@microsoft.com>
2016-10-10 14:05:59 -0700

    Connection string builder changes and adding EPH CIT ProcessorHostEntityPathSetting.

--- csharp/test/Microsoft.Azure.EventHubs.Processor.UnitTests/EventProcessorHostTests.cs
+++ csharp/test/Microsoft.Azure.EventHubs.Processor.UnitTests/EventProcessorHostTests.cs
@@ -13,7 +13,7 @@
 
     public class EventProcessorHostTests
     {
-        EventHubsConnectionSettings ConnectionSettings;
+        EventHubsConnectionStringBuilder ConnectionStringBuilder;
         string StorageConnectionString;
         string EventHubConnectionString;
         string LeaseContainerName;
@@ -33,22 +33,102 @@
                 throw new InvalidOperationException("EVENTPROCESSORSTORAGECONNECTIONSTRING environment variable was not found!");
             }
 
-            this.ConnectionSettings = new EventHubsConnectionSettings(eventHubConnectionString);
+            this.ConnectionStringBuilder = new EventHubsConnectionStringBuilder(eventHubConnectionString);
             this.StorageConnectionString = storageConnectionString;
             this.EventHubConnectionString = eventHubConnectionString;
 
             // Use entity name as lease container name.
-            this.LeaseContainerName = this.ConnectionSettings.EntityPath;
+            this.LeaseContainerName = this.ConnectionStringBuilder.EntityPath;
 
             // Discover partition ids.
-            PartitionIds = this.GetPartitionIdsAsync(this.ConnectionSettings).Result;
+            PartitionIds = this.GetPartitionIdsAsync(this.ConnectionStringBuilder.ToString()).Result;
             WriteLine($"EventHub has {PartitionIds.Length} partitions");
+        }
+
+        /// <summary>
+        /// Validating cases where entity path is provided through eventHubPath and EH connection string parameters
+        /// on the EPH constructor.
+        /// </summary>
+        [Fact]
+        void ProcessorHostEntityPathSetting()
+        {
+            var csb = new EventHubsConnectionStringBuilder(this.EventHubConnectionString);
+            csb.EntityPath = "myeh";
+
+            // Entity path provided in the connection string.
+            WriteLine("Testing condition: Entity path provided in the connection string only.");
+            var eventProcessorHost = new EventProcessorHost(
+                null,
+                PartitionReceiver.DefaultConsumerGroupName,
+                csb.ToString(),
+                this.StorageConnectionString,
+                this.LeaseContainerName);
+            Assert.Equal("myeh", eventProcessorHost.EventHubPath);
+
+            // Entity path provided in the eventHubPath parameter.
+            WriteLine("Testing condition: Entity path provided in the eventHubPath only.");
+            csb.EntityPath = null;
+            eventProcessorHost = new EventProcessorHost(
+                "myeh2",
+                PartitionReceiver.DefaultConsumerGroupName,
+                csb.ToString(),
+                this.StorageConnectionString,
+                this.LeaseContainerName);
+            Assert.Equal("myeh2", eventProcessorHost.EventHubPath);
+
+            // The same entity path provided in both eventHubPath parameter and the connection string.
+            WriteLine("Testing condition: The same entity path provided in the eventHubPath and connection string.");
+            csb.EntityPath = "mYeH";
+            eventProcessorHost = new EventProcessorHost(
+                "myeh",
+                PartitionReceiver.DefaultConsumerGroupName,
+                csb.ToString(),
+                this.StorageConnectionString,
+                this.LeaseContainerName);
+            Assert.Equal("myeh", eventProcessorHost.EventHubPath);
+
+            // Entity path not provided in both eventHubPath and the connection string.
+            WriteLine("Testing condition: Entity path not provided in both eventHubPath and connection string.");
+            try
+            {
+                csb.EntityPath = null;
+                eventProcessorHost = new EventProcessorHost(
+                    string.Empty,
+                    PartitionReceiver.DefaultConsumerGroupName,
+                    csb.ToString(),
+                    this.StorageConnectionString,
+                    this.LeaseContainerName);
+                throw new Exception("Entity path wasn't provided and this new call was supposed to fail");
+            }
+            catch (ArgumentException)
+            {
+                WriteLine("Caught ArgumentException as expected.");
+            }
+
+            // Entity path conflict.
+            WriteLine("Testing condition: Entity path conflict.");
+            try
+            {
+                csb.EntityPath = "myeh";
+                eventProcessorHost = new EventProcessorHost(
+                    "myeh2",
+                    PartitionReceiver.DefaultConsumerGroupName,
+                    csb.ToString(),
+                    this.StorageConnectionString,
+                    this.LeaseContainerName);
+                throw new Exception("Entity path values conflict and this new call was supposed to fail");
+            }
+            catch (ArgumentException)
+            {
+                WriteLine("Caught ArgumentException as expected.");
+            }
         }
 
         [Fact]
         async Task SingleProcessorHost()
         {
             var eventProcessorHost = new EventProcessorHost(
+                null,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -61,6 +141,7 @@
         async Task HostReregisterShouldFail()
         {
             var eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -111,6 +192,7 @@
             {
                 WriteLine($"Creating EventProcessorHost");
                 var eventProcessorHost = new EventProcessorHost(
+                    string.Empty,
                     PartitionReceiver.DefaultConsumerGroupName,
                     this.EventHubConnectionString,
                     this.StorageConnectionString,
@@ -154,7 +236,7 @@
             var sendTasks = new List<Task>();
             foreach (var partitionId in PartitionIds)
             {
-                sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionSettings));
+                sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionStringBuilder.ToString()));
             }
             await Task.WhenAll(sendTasks);
 
@@ -190,6 +272,7 @@
             }
 
             var eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -244,6 +327,7 @@
             WriteLine($"Calling RegisterEventProcessorAsync with InvokeProcessorAfterReceiveTimeout=false");
 
             var eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -324,6 +408,7 @@
             foreach (var consumerGroupName in consumerGroupNames)
             {
                 var eventProcessorHost = new EventProcessorHost(
+                    string.Empty,
                     consumerGroupName,
                     this.EventHubConnectionString,
                     this.StorageConnectionString,
@@ -344,7 +429,7 @@
             var sendTasks = new List<Task>();
             foreach (var partitionId in PartitionIds)
             {
-                sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionSettings));
+                sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionStringBuilder.ToString()));
             }
 
             await Task.WhenAll(sendTasks);
@@ -381,6 +466,7 @@
 
             // Use a randomly generated container name so that initial offset provider will be respected.
             var eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -406,6 +492,7 @@
 
             // Use a randomly generated container name so that initial offset provider will be respected.
             var eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -431,6 +518,7 @@
 
             // First host will send and receive as usual.
             var eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -441,6 +529,7 @@
             // Since we are still on the same lease container, initial offset provider shouldn't rule.
             // We should continue receiving where we left instead if start-of-stream where initial offset provider dictates.
             eventProcessorHost = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -464,6 +553,7 @@
 
             // Consume all messages with first host.
             var eventProcessorHostFirst = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -473,6 +563,7 @@
             // For the second time we initiate a host and this time it should pick from where the previous host left.
             // In other words, it shouldn't start receiving from start of the stream.
             var eventProcessorHostSecond = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -491,6 +582,7 @@
 
             // Consume all messages with first host.
             var eventProcessorHostFirst = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -500,6 +592,7 @@
             // For the second time we initiate a host and this time it should pick from where the previous host left.
             // In other words, it shouldn't start receiving from start of the stream.
             var eventProcessorHostSecond = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -522,6 +615,7 @@
 
             // Consume all messages with first host.
             var eventProcessorHostFirst = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -532,6 +626,7 @@
             // Seconds time we initiate a host, it should pick from where previous host left.
             // In other words, it shouldn't start receiving from start of the stream.
             var eventProcessorHostSecond = new EventProcessorHost(
+                string.Empty,
                 PartitionReceiver.DefaultConsumerGroupName,
                 this.EventHubConnectionString,
                 this.StorageConnectionString,
@@ -551,13 +646,13 @@
             var sendTasks = new List<Task>();
             foreach (var partitionId in PartitionIds)
             {
-                sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionSettings));
+                sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionStringBuilder.ToString()));
             }
 
             await Task.WhenAll(sendTasks);
 
             // Receive all events including last events from each partition.
-            var ehClient = EventHubClient.Create(this.ConnectionSettings);
+            var ehClient = EventHubClient.CreateFromConnectionString(this.ConnectionStringBuilder.ToString());
             ConcurrentDictionary<string, EventData> lastEvents = new ConcurrentDictionary<string, EventData>();
             var receiveTasks = PartitionIds.Select(async partitionId =>
                 {
@@ -639,7 +734,7 @@
                 {
                     for (int i = 0; i < totalNumberOfEventsToSend; i++)
                     {
-                        sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionSettings));
+                        sendTasks.Add(this.SendToPartitionAsync(partitionId, $"{partitionId} event.", this.ConnectionStringBuilder.ToString()));
                     }
                 }
 
@@ -668,9 +763,9 @@
             return receivedEvents.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
         }
 
-        async Task<string[]> GetPartitionIdsAsync(EventHubsConnectionSettings connectionSettings)
+        async Task<string[]> GetPartitionIdsAsync(string connectionString)
         {
-            var eventHubClient = EventHubClient.Create(connectionSettings);
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString);
             try
             {
                 var eventHubInfo = await eventHubClient.GetRuntimeInformationAsync();
@@ -682,9 +777,9 @@
             }
         }
 
-        async Task SendToPartitionAsync(string partitionId, string messageBody, EventHubsConnectionSettings connectionSettings)
+        async Task SendToPartitionAsync(string partitionId, string messageBody, string connectionString)
         {
-            var eventHubClient = EventHubClient.Create(connectionSettings);
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString);
             try
             {
                 var partitionSender = eventHubClient.CreatePartitionSender(partitionId);
