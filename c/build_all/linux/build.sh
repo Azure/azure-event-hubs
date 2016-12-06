d66d609d40f20f514f42a298e5aa98daeeea7129
Dan Cristoloveanu <dcristo@microsoft.com>
2016-12-06 10:49:19 -0800

    Modify skip_unittests to be run_unittests and default running unit tests to OFF

--- c/build_all/linux/build.sh
+++ c/build_all/linux/build.sh
@@ -9,7 +9,7 @@ build_root=$(cd "${script_dir}/../.." && pwd)
 log_dir=$build_root
 run_e2e_tests=OFF
 run_longhaul_tests=OFF
-skip_unittests=OFF
+run_unittests=OFF
 
 usage ()
 {
@@ -18,7 +18,7 @@ usage ()
     echo " -cl, --compileoption <value>  specify a compile option to be passed to gcc"
     echo "   Example: -cl -O1 -cl ..."
     echo " --run-e2e-tests               runs the end-to-end tests (e2e tests are not run by default)"
-    echo " --skip-unittests              skip the running of unit tests (unit tests are run by default)"
+    echo " --run-unittests               run the unit tests"
     exit 1
 }
 
@@ -38,7 +38,7 @@ process_args ()
           case "$arg" in
               "-cl" | "--compileoption" ) save_next_arg=1;;
               "--run-e2e-tests" ) run_e2e_tests=ON;;
-              "--skip-unittests" ) skip_unittests=ON;;
+              "--run-unittests" ) run_unittests=ON;;
               * ) usage;;
           esac
       fi
@@ -50,7 +50,7 @@ process_args $*
 rm -r -f ~/cmake
 mkdir ~/cmake
 pushd ~/cmake
-cmake -DcompileOption_C:STRING="$extracloptions" -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Dskip_unittests:BOOL=$skip_unittests $build_root
+cmake -DcompileOption_C:STRING="$extracloptions" -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Drun_unittests:BOOL=$run_unittests $build_root
 make --jobs=$(nproc)
 ctest -C "Debug" -V
 popd
