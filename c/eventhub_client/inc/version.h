f95a353b75b66a9ca41ad5e430bd94ef5d8e5565
mrohera <marohera@microsoft.com>
2016-12-08 16:59:00 -0800

    Added version number to EH C SDK

--- c/eventhub_client/inc/version.h
+++ c/eventhub_client/inc/version.h
@@ -4,6 +4,8 @@
 #ifndef EVENTHUBVERSION_H
 #define EVENTHUBVERSION_H
 
+#define EVENT_HUB_SDK_VERSION "0.9.0"
+
 #include "azure_c_shared_utility/umock_c_prod.h"
 
 #ifdef __cplusplus
--- c/eventhub_client/src/version.c
+++ c/eventhub_client/src/version.c
@@ -3,15 +3,8 @@
 
 #include "version.h"
 
-#define TOSTRING_(x) #x
-#define TOSTRING(x) TOSTRING_(x)
-
-#ifndef EHC_VERSION
-#define EHC_VERSION unknown
-#endif
-
 const char* EventHubClient_GetVersionString(void)
 {
     /* Codes_SRS_EVENTHUBCLIENT_05_003: [EventHubClient_GetVersionString shall return a pointer to a constant string which indicates the version of EventHubClient API.] */
-    return TOSTRING(EHC_VERSION);
+    return EVENT_HUB_SDK_VERSION;
 }
