5728b71650812d577f0b2365bfa71afdb1cae0f8
shvija <shvija@microsoft.com>
2018-02-05 20:32:17 -0800

    Adding Big data streaming and processing sample using EH and ASA

---
+++ samples/DotNet/AnomalyDetector/Producer/TransactionType.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Producer
+{
+    /// <summary>
+    /// A mechanism to identify the mock transactions that are generated on the client side.
+    /// This is not used downstream. But is used more for an easy (or visual) comparison of
+    /// anomalous data that the downstream analytics produces.
+    /// </summary>
+    internal enum TransactionType
+    {
+        Regular,
+        Suspect,
+    }
+}
+
