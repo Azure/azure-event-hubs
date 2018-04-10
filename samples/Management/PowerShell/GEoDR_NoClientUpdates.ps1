c3a44ff9e2cfe40d7ce2748c12fdd4aaf67a03f2
shvija <shvija@microsoft.com>
2018-04-10 16:32:46 -0700

    removing the CLI parts

--- samples/Management/PowerShell/GEoDR_NoClientUpdates.ps1
+++ samples/Management/PowerShell/GEoDR_NoClientUpdates.ps1
@@ -1,44 +1,44 @@
-Following are the steps required to set up a geo-dr configuration with the existing connectiong strings
+#Step1 - Login to you Azure subscription
+Login-AzureRmAccount
 
-#Step1 - Login to your Azure subscription
-az login
+#Option - set you desired subscription
+Set-AzureRmContext -Subscription "your subscription name"
 
-#Optional - set your desired subscription
-az account set --subscription "your subscription name"
+#Step2 - Following parameters are used while creating the resources
+#geo location where the primary namespace is
+$location1="SouthCentralUS"
 
-#Step 2 – following parameters are used while creating the resources. 
+#geo location where the secondary namespace should be created
+$location2="NorthCentralUS"
 
-#geo location for the primarynamespace
-export location1=SouthCentralUS
-
-#geo location where the secondarynamespace should be created
-export location2=NorthCentralUS
-
-#provide your exisitng resourcegroup which has the primarynamespace
-export resourcegroup=myresourcegroup
+#provide your existing resourcegroup which has the primary namespace
+$resourceGroup = “<provide your resource group name>”
 
 #provide your existing primary namespace
-export primarynamespace=myexistingprimarynamespace
+$primarynamespace = “<provide your primary namespace name>”
 
-export secondarynamespace=myexistingsecondarynamespace
-# here your primarynamespace is your alias name
-export aliasname=myexistingprimarynamespace
-export alternatename=testehalternatename1
+$secondarynamespace = “<provide your secondary namespace name>”
+
+#your primary namespace is you alias here
+$aliasname = “<provide your primary namespace name>”
+
+$alternatename = "<your alternatename for you primary namespace>"
 
 #Step 3 - create your secondary namespace. Copy the ARM ID from the output as you need it later for -PartnerPartnernamespace. 
 # sample ARM ID looks like this - /subscriptions/your subscriptionid/resourceGroups/$resourcegroup/providers/Microsoft.EventHub/namespaces/secondarynamespace
-az eventhubs namespace create --name $secondarynamespace --resource-group $resourcegroup --location $location2 --sku Standard
+New-AzureRmEventHubNamespace – ResourceGroup $resourceGroup – NamespaceName $secondarynamespace -Location $location2 -SkuName Standard
 
 #Step 4 - Create a geo-dr configuration with primarynamespace name as the alias name and pair the namespaces. Note, you will also provide the alternatename here.
 # It is using this alternatename that you will access your old primary once you have triggered the failover
-az eventhubs georecovery-alias set --resource-group $resourcegroup --alias $primarynamespace --namespace-name $primarynamespace --partner-namespace "ARM ID of secondary namespace" --alternate-name $alternatename
+New-AzureRmEventHubGeoDRConfiguration -Name $aliasname -Namespace $primarynamespace -PartnerNamespace "your ARM ID here" -ResourceGroupName $resourcegroup -AlternateName $alternatename 
 
-#Optional - Once your geo-dr configurations are set, you can get the details using this
-az eventhubs georecovery-alias show --resource-group $resourcegroup --alias $aliasname --namespace-name $primarynamespace
+#Optional - Once your geo-dr configurations are set, you can get the details using this'
+Get-AzureRmEventHubGeoDRConfiguration -ResourceGroup $resourcegroup -Name $aliasname -Namespace $primarynamespace
 
 #Step 5 - you can now do a failover on your secondary namespace with the following command
-az eventhubs georecovery-alias fail-over --resource-group $resourcegroup --alias $aliasname --namespace-name $secondarynamespace
+Set-AzureRmEventHubGeoDRConfigurationFailOver -ResourceGreoup $resourcegroup -Name $aliasname -Namespace $secondarynamespace
 
 #Optional - check you geo-dr configuration details to reflect the fail-over
 #Note - your secondarynamespace after the failover will be your new primary, but the connection string remains the same
-az eventhubs georecovery-alias show --resource-group $resourcegroup --alias $aliasname --namespace-name $secondarynamespace #this is your current primary
+Get-AzureRmEventHubGeoDRConfiguration -ResourceGroup $resourcegroup -Name $aliasname -Namespace $secondarynamespace 
+
