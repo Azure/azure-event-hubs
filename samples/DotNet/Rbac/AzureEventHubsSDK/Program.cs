5ca42a24a6dec5ce6efaf54a8b2e2cf9b56a33d3
Serkant Karaca <serkar@microsoft.com>
2018-09-28 12:44:35 -0700

    Couple improvements in the send/receive flow

--- samples/DotNet/Rbac/AzureEventHubsSDK/Program.cs
+++ samples/DotNet/Rbac/AzureEventHubsSDK/Program.cs
@@ -12,7 +12,6 @@ namespace EventHubsSenderReceiverRbac
 {
     class Program
     {
-        
         static readonly string TenantId = ConfigurationManager.AppSettings["tenantId"];
         static readonly string ClientId = ConfigurationManager.AppSettings["clientId"]; 
         static readonly string EventHubNamespace = ConfigurationManager.AppSettings["eventHubNamespaceFQDN"];
@@ -125,11 +124,14 @@ namespace EventHubsSenderReceiverRbac
 
         static async Task SendReceiveAsync(EventHubClient ehClient)
         {
+            Console.WriteLine("Fetching eventhub description to discover partitions");
             var ehDesc = await ehClient.GetRuntimeInformationAsync();
+            Console.WriteLine($"Discovered partitions as {string.Join(",", ehDesc.PartitionIds)}");
 
             var receiveTasks = ehDesc.PartitionIds.Select(async partitionId =>
                     {
-                        var receiver = ehClient.CreateReceiver(PartitionReceiver.DefaultConsumerGroupName, partitionId, EventPosition.FromStart());
+                        Console.WriteLine($"Initiating receiver on partition {partitionId}");
+                        var receiver = ehClient.CreateReceiver(PartitionReceiver.DefaultConsumerGroupName, partitionId, EventPosition.FromEnd());
 
                         while(true)
                         {
@@ -140,21 +142,25 @@ namespace EventHubsSenderReceiverRbac
                             }
 
                             var eventData = events.FirstOrDefault();
-                            Console.WriteLine($"Received from partition {partitionId} : " + Encoding.UTF8.GetString(eventData.Body.Array));                            
+                            Console.WriteLine($"Received from partition {partitionId} with message content '" + Encoding.UTF8.GetString(eventData.Body.Array) + "'"); 
                         }
 
                         await receiver.CloseAsync();
-                    });
+                    }).ToList<Task>();
 
             await Task.Delay(5000);
 
+            Console.WriteLine("Sending single event");
             await ehClient.SendAsync(new EventData(Encoding.UTF8.GetBytes($"{DateTime.UtcNow}")));
+            Console.WriteLine("Send done");
 
+            Console.WriteLine("Waiting for receivers to complete");
             await Task.WhenAll(receiveTasks);
+            Console.WriteLine("All receivers completed");
 
             await ehClient.CloseAsync();
 
-            Console.WriteLine("Send / Receive complete. Press enter to exit.");
+            Console.WriteLine("Press enter to exit.");
 
             Console.ReadLine();
         }
