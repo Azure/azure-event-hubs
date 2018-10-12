86b95b618a3c91820fb10c7bf1fbe495b16d3c96
Garrett Edwards <grrttedwards@users.noreply.github.com>
2018-10-12 10:29:57 -0400

    Update producer.py

--- samples/kafka/python/producer.py
+++ samples/kafka/python/producer.py
@@ -34,11 +34,11 @@ if __name__ == '__main__':
     # See https: //github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
     # See https://github.com/edenhill/librdkafka/wiki/Using-SSL-with-librdkafka#prerequisites for SSL issues
     conf = {
-        'bootstrap.servers': '{YOUR.EVENTHUB.FQDN}:9093',
+        'bootstrap.servers': '{YOUR.EVENTHUBS.FQDN}:9093',
         'security.protocol': 'SASL_SSL',
         'sasl.mechanism': 'PLAIN',
         'sasl.username': '$ConnectionString',
-        'sasl.password': '{YOUR.EVENTHUB.CONNECTION.STRING}',
+        'sasl.password': '{YOUR.EVENTHUBS.CONNECTION.STRING}',
         'client.id': 'python-example-producer'
     }
 
