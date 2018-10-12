0089580e1b53606a4f6f91955a2f7502d1feeb22
Garrett Edwards <grrttedwards@users.noreply.github.com>
2018-10-12 09:58:39 -0400

    Fix style in producer.py

--- samples/kafka/python/producer.py
+++ samples/kafka/python/producer.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 #
-#Copyright 2016 Confluent Inc.
-# 
+# Copyright 2016 Confluent Inc.
+#
 # Modified for use with Azure Event Hubs for Apache Kafka Ecosystems
 # 06/18/2018
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 #
-#Example Kafka Producer.
+# Example Kafka Producer.
 #
 from confluent_kafka import Producer
 import sys
@@ -30,9 +30,9 @@ if __name__ == '__main__':
         sys.exit(1)
     topic = sys.argv[1]
 
-    #Producer configuration
-    #See https: //github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
-    #See https://github.com/edenhill/librdkafka/wiki/Using-SSL-with-librdkafka#prerequisites for SSL issues
+    # Producer configuration
+    # See https: //github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    # See https://github.com/edenhill/librdkafka/wiki/Using-SSL-with-librdkafka#prerequisites for SSL issues
     conf = {
         'bootstrap.servers': '{YOUR.EVENTHUB.FQDN}:9093',
         'security.protocol': 'SASL_SSL',
@@ -42,25 +42,25 @@ if __name__ == '__main__':
         'client.id': 'python-example-producer'
     }
 
-    #Create Producer instance
-    p = Producer (**conf)
+    # Create Producer instance
+    p = Producer(**conf)
 
-    def delivery_callback (err, msg):
+
+    def delivery_callback(err, msg):
         if err:
-            sys.stderr.write ('%% Message failed delivery: %s\n' % err)
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
         else:
-            sys.stderr.write ('%% Message delivered to %s [%d] @ %o\n' % (msg.topic (), msg.partition (), msg.offset ()))
+            sys.stderr.write('%% Message delivered to %s [%d] @ %o\n' % (msg.topic(), msg.partition(), msg.offset()))
 
 
-    #Write 1-100 to topic
-    for i in range(0,100):
+    # Write 1-100 to topic
+    for i in range(0, 100):
         try:
             p.produce(topic, str(i), callback=delivery_callback)
         except BufferError as e:
-            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len (p))
-        p.poll (0)
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len(p))
+        p.poll(0)
 
-    #Wait until all messages have been delivered
-    sys.stderr.write ('%% Waiting for %d deliveries\n' % len (p))
-    p.flush ()
-    
+    # Wait until all messages have been delivered
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
