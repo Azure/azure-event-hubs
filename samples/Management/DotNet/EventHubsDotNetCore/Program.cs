a96175d817b68a0027cabc4c0f392a2216ce82e2
Prathamesh Govekar (MINDTREE LIMITED) <v-prgov@microsoft.com>
2019-02-25 15:00:43 -0800

    Added new EventHubsDotNetCoreSample project

---
+++ samples/Management/DotNet/EventHubsDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventHubsDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventHubManagementSample.Run();
+        }
+    }
+}
