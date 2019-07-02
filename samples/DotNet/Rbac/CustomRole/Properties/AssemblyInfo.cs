10ec29a21907c96477cede128c9fb279e90d3c35
Serkant Karaca <serkar@microsoft.com>
2019-07-02 09:33:53 -0700

    Reverting unintended changes

--- samples/DotNet/Rbac/CustomRole/Properties/AssemblyInfo.cs
+++ samples/DotNet/Rbac/CustomRole/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SampleReceiver")]
+[assembly: AssemblyProduct("CustomRole")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
